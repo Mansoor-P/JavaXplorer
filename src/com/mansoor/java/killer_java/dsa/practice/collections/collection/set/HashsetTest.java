8b37a0dea4b860a7872ca15013d742d8b1a97b9c
MansoorP <103396101+Mansoor-P@users.noreply.github.com>
2023-08-16 19:59:45 +0530

    set practicing

--- src/com/mansoor/java/killer_java/dsa/practice/collections/collection/set/HashsetTest.java
+++ src/com/mansoor/java/killer_java/dsa/practice/collections/collection/set/HashsetTest.java
@@ -1,14 +1,39 @@
-package dsa.practice.collections.collection.set;
+package com.mansoor.java.killer_java.dsa.practice.collections.collection.set;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class HashsetTest {
     public static void main(String[] args) {
-        Set<String> hs = new HashSet<>();
-        hs.add("MANSOOR");
-        hs.add("MANSO7R");
-        System.out.println(hs);
+//        Set<String> hs = new HashSet<>();
+//        hs.add("MANSOOR");
+//        hs.add("MANSO7R");
+//        System.out.println(hs);
+
+
+        List<String> L = new ArrayList<String>();
+
+        // Add values to the List
+        L.add("Aragorn");
+        L.add("Gandalf");
+        L.add("Legolas");
+        L.add("Frodo");
+
+        // Create a Set and pass List object as parameter
+        HashSet<String> S = new HashSet<String>(L);
+
+        // Print values of Set
+        System.out.println("HashSet Elements are : ");
+
+        // since the set is of string type, create a String
+        // object to iterate through set
+        for (String ob : S)
+        {
+            System.out.println(ob);
+        }
     }
+
 
 }
