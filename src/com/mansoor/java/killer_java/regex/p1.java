d0bae0aa90724217f36bbde4c4afd6687966e0ae
MansoorP <103396101+Mansoor-P@users.noreply.github.com>
2023-08-16 19:56:14 +0530

    java regex practicing

---
+++ src/com/mansoor/java/killer_java/regex/p1.java
@@ -0,0 +1,31 @@
+package com.mansoor.java.killer_java.regex;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class p1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Your word");
+        String n=sc.next();
+
+//        System.out.println(Pattern.matches("[bcr]at",n));
+
+//        System.out.println(Pattern.matches("[^bcr]at",n));
+
+//        System.out.println(Pattern.matches("foo[1-5]",n));
+//
+//        System.out.println(Pattern.matches("foo[^1-5]",n));
+
+        // " .,\d,\D,\s,\S,\w,\W"
+
+//        System.out.println(Pattern.matches("\\W" ,n));
+
+        // <? * + {n} {n,} {n,m}>
+//        System.out.println(Pattern.matches("m{4,7}",n));
+//          System.out.println(Pattern.matches("man{2}",n));
+
+
+
+    }
+}
