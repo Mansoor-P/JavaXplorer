9b49bdd526e2746af11828ccf7b0b21dac6b475c
MansoorP <103396101+Mansoor-P@users.noreply.github.com>
2023-08-18 21:44:00 +0530

    funtional programing started with functional interfaces

---
+++ src/com/mansoor/java/killer_java/functionalprograming/main.java
@@ -0,0 +1,17 @@
+package com.mansoor.java.killer_java.functionalprograming;
+
+@FunctionalInterface  // functional interface has only one abstract method
+public interface main {
+     void sum();
+     default void light(){
+
+     }
+     private void dark(){
+
+     }
+     static  void defaulttheame(){
+
+     }
+
+
+}
