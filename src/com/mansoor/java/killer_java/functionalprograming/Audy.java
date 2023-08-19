e298b3a4ff1a8f35b0eef601bfec27df7dc2a82c
MansoorP <103396101+Mansoor-P@users.noreply.github.com>
2023-08-19 08:20:42 +0530

    lamda expressions

---
+++ src/com/mansoor/java/killer_java/functionalprograming/Audy.java
@@ -0,0 +1,9 @@
+package com.mansoor.java.killer_java.functionalprograming;
+
+public class Audy  implements Car {
+
+    @Override
+    public void drive() {
+        System.out.println(" audi Drive is called");
+    }
+}
---
+++ src/com/mansoor/java/killer_java/functionalprograming/Car.java
@@ -0,0 +1,9 @@
+package com.mansoor.java.killer_java.functionalprograming;
+
+
+
+@FunctionalInterface  // functional interface has only one abstract method
+public interface Car {
+     void drive();
+
+}
