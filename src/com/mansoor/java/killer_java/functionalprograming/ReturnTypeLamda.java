67e4a73a90ebb48250e928c1fe498ad4c79b1829
MansoorP <103396101+Mansoor-P@users.noreply.github.com>
2023-08-19 08:42:56 +0530

    single return type lamdaexpression

---
+++ src/com/mansoor/java/killer_java/functionalprograming/ReturnTypeLamda.java
@@ -0,0 +1,39 @@
+package com.mansoor.java.killer_java.functionalprograming;
+interface Laptop{
+    public int usbports();
+}
+class Vostro implements Laptop{
+
+    @Override
+    public int usbports() {
+        int speed=100;
+        System.out.println("from class" +speed);
+        return speed;
+    }
+}
+public class ReturnTypeLamda {
+    public static void main(String[] args) {
+        Vostro v=new Vostro();
+        v.usbports();
+
+        Laptop l=new Laptop() {
+            @Override
+            public int usbports() {
+                int speed=50;
+                System.out.println("Return from anonymous class" + speed);
+                return speed;
+            }
+        };
+        l.usbports();
+
+        Laptop lp = () -> {
+            int speed=200;
+            System.out.println("drm lamda expressions" + speed);
+        return speed;
+        };
+        lp.usbports();
+
+
+    }
+}
+
