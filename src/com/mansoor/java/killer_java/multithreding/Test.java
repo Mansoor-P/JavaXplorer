fddeae14d357d4d71e10ae4f9656dc765445f463
MansoorP <103396101+Mansoor-P@users.noreply.github.com>
2023-08-20 12:54:15 +0530

    multiple threads executing by implementing runnable interface

---
+++ src/com/mansoor/java/killer_java/multithreding/Test.java
@@ -0,0 +1,26 @@
+package com.mansoor.java.killer_java.multithreding;
+
+public class Test {
+    public static void main(String[] args) {
+    User u=new User();
+    Thread t1=new Thread(u);
+        t1.start();
+        HR hr=new HR();
+        Thread t2=new Thread(hr);
+        t2.start();
+    }
+}
+
+class User implements  Runnable{
+    @Override
+    public void run(){
+        System.out.println("User Runnable");
+    }
+}
+class HR implements Runnable{
+    @Override
+    public void run(){
+        System.out.println("HR runnable");
+    }
+}
+
