c4b1e4295dbab2ea12c61becda9af4364e8888ba
MansoorP <103396101+Mansoor-P@users.noreply.github.com>
2023-08-20 12:32:59 +0530

    Employee class extending Thread class and @overriding run method and counting the how many current threds are working

--- src/com/mansoor/java/killer_java/multithreding/Main.java
+++ src/com/mansoor/java/killer_java/multithreding/Main.java
@@ -2,9 +2,14 @@ package com.mansoor.java.killer_java.multithreding;
 
 public class Main {
     public static void main(String[] args) {
+
         Thread t1=new Thread();
         t1.start();
         System.out.println(Thread.currentThread().getName());
+
+        Employee e1=new Employee();
+        e1.start();
+        System.out.println(Thread.activeCount());
     }
 }
 class Employee extends  Thread{
