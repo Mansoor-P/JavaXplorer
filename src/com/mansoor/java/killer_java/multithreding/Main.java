7e0a7e937ef64567c883b47d3abcb93eed5b133b
MansoorP <103396101+Mansoor-P@users.noreply.github.com>
2023-08-20 12:31:24 +0530

    Employee class extending Thread class and @overriding run method

--- src/com/mansoor/java/killer_java/multithreding/Main.java
+++ src/com/mansoor/java/killer_java/multithreding/Main.java
@@ -2,7 +2,15 @@ package com.mansoor.java.killer_java.multithreding;
 
 public class Main {
     public static void main(String[] args) {
-
+        Thread t1=new Thread();
+        t1.start();
         System.out.println(Thread.currentThread().getName());
     }
+}
+class Employee extends  Thread{
+    @Override
+    public void run(){
+        System.out.println("run method from employee");
+    }
+
 }
