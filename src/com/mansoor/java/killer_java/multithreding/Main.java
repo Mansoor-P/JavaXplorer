d895a28a12466d6bfdbf8d842beeca6c6c5747c5
MansoorP <103396101+Mansoor-P@users.noreply.github.com>
2023-08-20 12:24:42 +0530

    finding thread name

---
+++ src/com/mansoor/java/killer_java/multithreding/Main.java
@@ -0,0 +1,7 @@
+package com.mansoor.java.killer_java.multithreding;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName());
+    }
+}
