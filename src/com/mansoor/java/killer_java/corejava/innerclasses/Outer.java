fbf74d9d857305d4ff73349f8f4abf32cee2da0b
MansoorP <103396101+Mansoor-P@users.noreply.github.com>
2023-08-23 23:01:04 +0530

    Inner classes practicing

---
+++ src/com/mansoor/java/killer_java/corejava/innerclasses/Outer.java
@@ -0,0 +1,12 @@
+package com.mansoor.java.killer_java.corejava.innerclasses;
+
+public class Outer {
+    public static void main(String[] args) {
+        System.out.println("from Outer");
+    }
+    class Inner{
+        public static void main(String[] args) {
+            System.out.println("From Inner");
+        }
+    }
+}
