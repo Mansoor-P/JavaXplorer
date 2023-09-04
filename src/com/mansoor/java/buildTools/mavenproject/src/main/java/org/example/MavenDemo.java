8a93f9d542a563605af172d3d0dca76446672fdb
MunafVlog <munaf6007@gmail.com>
2023-09-04 09:43:33 +0530

    maven demo

---
+++ src/com/mansoor/java/buildTools/mavenproject/src/main/java/org/example/MavenDemo.java
@@ -0,0 +1,7 @@
+package com.mansoor.java.buildTools.mavenproject.src.main.java.org.example;
+
+public class MavenDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
