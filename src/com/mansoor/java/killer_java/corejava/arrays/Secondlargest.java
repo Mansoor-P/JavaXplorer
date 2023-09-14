db70a2533e0b947f0348baf48b6679d81bcb3f11
MunafVlog <munaf6007@gmail.com>
2023-09-14 22:00:44 +0530

    second largest number finding in an array

---
+++ src/com/mansoor/java/killer_java/corejava/arrays/Secondlargest.java
@@ -0,0 +1,38 @@
+package com.mansoor.java.killer_java.corejava.arrays;
+
+import java.util.Scanner;
+
+public class Secondlargest {
+
+    static int findmax(int arr[]){
+        int mx=Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]>mx){
+                mx=arr[i];
+            }
+        }
+        return  mx;
+    }
+    static int findsecondmax(int arr[]){
+        int mx=findmax(arr);
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==mx){
+                arr[i]=Integer.MIN_VALUE;
+            }
+        }
+        int secondmax=findmax(arr);
+        return secondmax;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter array size");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+
+        System.out.println("Enter " + n + " Elements ");
+        for (int i = 0; i <n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("second largest is : "+findsecondmax(arr));
+    }
+}
