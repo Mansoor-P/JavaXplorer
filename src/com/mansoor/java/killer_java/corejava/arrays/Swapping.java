aec03dc4b57bb7c8ef160e2e9a48a374f8024858
MunafVlog <munaf6007@gmail.com>
2023-09-16 10:56:53 +0530

    swapping two variables

---
+++ src/com/mansoor/java/killer_java/corejava/arrays/Swapping.java
@@ -0,0 +1,17 @@
+package com.mansoor.java.killer_java.corejava.arrays;
+
+import java.util.Scanner;
+
+public class Swapping {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a and b values");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("Before Swapped " +a+" and "+b);
+        int temp =a;
+        a=b;
+        b=temp;
+        System.out.println("After Swapped "+a+" and "+b);
+    }
+}
