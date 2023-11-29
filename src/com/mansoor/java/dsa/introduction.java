faf3273b27e5a9fc1380cdc2f446d5f364501492
mansoor-P <munaf6007@gmail.com>
2023-11-29 16:52:01 +0530

    Count number of even and odd elements in an array

---
+++ src/com/mansoor/java/dsa/introduction.java
@@ -0,0 +1,17 @@
+package com.mansoor.java.dsa;
+
+public class introduction {
+    public static void main(String[] args) {
+        int arr[] ={ 2, 3, 4, 5, 6 };
+
+        int n= arr.length;
+
+        for (int i = 0; i <n ; i++) {
+            if (i%2!=0){
+                System.out.println(arr[i]);
+            }
+
+        }
+
+    }
+}
