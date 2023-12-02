0d89a245aac0e1b5bd615fd0d18a34ac474eb9e0
mansoor-P <munaf6007@gmail.com>
2023-12-02 10:19:52 +0530

    Type casting

---
+++ src/com/mansoor/java/killer_java/basics/Typecasting.java
@@ -0,0 +1,22 @@
+package com.mansoor.java.killer_java.basics;
+
+public class Typecasting {
+    static int i = 10; // Static variable
+    boolean b = true; // Instance variable
+
+    static double d = 20.128; // Keeping the type as double
+
+    public static void main(String[] args) {
+
+        short s = 2; // local variable
+
+        d = i;
+        System.out.println(d); // result 10.0
+
+        i = (int) d;
+
+        System.out.println(i); // result 10
+
+        System.out.println(d + " " + i);
+    }
+}
