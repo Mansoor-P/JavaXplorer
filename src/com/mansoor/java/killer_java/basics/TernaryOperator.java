be828c28aa54e61836a6f5daae28d95bd8930133
mansoor-P <munaf6007@gmail.com>
2023-12-02 10:57:05 +0530

    Ternary operator

---
+++ src/com/mansoor/java/killer_java/basics/TernaryOperator.java
@@ -0,0 +1,13 @@
+package com.mansoor.java.killer_java.basics;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int x=10;
+        int y=20;
+
+        int max=(x>y)?x:y;
+
+        System.out.println(max);
+    }
+
+}
