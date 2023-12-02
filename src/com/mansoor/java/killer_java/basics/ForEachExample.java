77428c95b805842c2dbe58ed0ef6c30db2686c33
mansoor-P <munaf6007@gmail.com>
2023-12-02 10:55:52 +0530

    for each loop

---
+++ src/com/mansoor/java/killer_java/basics/ForEachExample.java
@@ -0,0 +1,12 @@
+package com.mansoor.java.killer_java.basics;
+
+public class ForEachExample {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        // Using a for-each loop to iterate over the elements of the array
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
