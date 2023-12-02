cb8b038a7ba594c8fe466b451978691b5a2f493a
mansoor-P <munaf6007@gmail.com>
2023-12-02 10:56:19 +0530

    MethodOverriding

---
+++ src/com/mansoor/java/killer_java/basics/methods/MethodOverriding.java
@@ -0,0 +1,31 @@
+package com.mansoor.java.killer_java.basics.methods;
+class Animal {
+    public void makeSound() {
+        System.out.println("Some generic sound");
+    }
+}
+
+class Dog extends Animal {
+    // Method overriding - providing a specific implementation for makeSound in the Dog class
+    @Override
+    public void makeSound() {
+        System.out.println("Woof! Woof!");
+    }
+}
+
+class Cat extends Animal {
+    // Method overriding - providing a specific implementation for makeSound in the Cat class
+    @Override
+    public void makeSound() {
+        System.out.println("Meow!");
+    }
+}
+public class MethodOverriding {
+    public static void main(String[] args) {
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        dog.makeSound(); // Calls the makeSound method in the Dog class
+        cat.makeSound(); // Calls the makeSound method in the Cat class
+    }
+}
