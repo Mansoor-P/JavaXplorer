87bb0096d2aa785298015590890f4a8d8b03b617
mansoor-P <munaf6007@gmail.com>
2023-12-05 22:24:28 +0530

    Printing Natural Numbers using recursion

--- src/com/mansoor/java/killer_java/dsa/recurssion/NatualNumbers.java
+++ src/com/mansoor/java/killer_java/dsa/recurssion/NatualNumbers.java
@@ -1,5 +1,7 @@
 package com.mansoor.java.killer_java.dsa.recurssion;
 
+import java.util.Scanner;
+
 public class NatualNumbers {
     public static void sumofNumbers(int i,int n,int sum){
 
@@ -9,7 +11,20 @@ public class NatualNumbers {
         sum=n*(n+1)/2;
         System.out.println(sum);
     }
+
+    static void printIncresing(int n){  // 1,2,3.....n-1,n
+        if(n==1){
+            System.out.println(n);  // base case
+            return;
+        }
+        printIncresing(n-1); // 1,2,3.....n-1
+        System.out.println(n);
+    }
+
     public static void main(String[] args) {
-        sumofNumbers(1,5,0);
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        printIncresing(n);
+//        sumofNumbers(1,5,0);
     }
 }
