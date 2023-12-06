76daf21d038ae3c65f23fba44c6cd1729c669842
mansoor-P <munaf6007@gmail.com>
2023-12-06 16:03:32 +0530

    getters and setters

---
+++ src/com/mansoor/java/killer_java/OOPS/Student.java
@@ -0,0 +1,15 @@
+package com.mansoor.java.killer_java.OOPS;
+
+public  class Student{
+    String name;
+   int rollNo;
+    double percent;
+
+    public int getRollNo() {   // getter
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {  // setter
+        rollNo = rollNo;
+    }
+}
--- src/com/mansoor/java/killer_java/OOPS/StudentClass.java
+++ src/com/mansoor/java/killer_java/OOPS/StudentClass.java
@@ -1,24 +1,16 @@
 package com.mansoor.java.killer_java.OOPS;
 
 public class StudentClass {
-    // creating a new data type with multiple attributes
-    public static class Student{
-        String name;
-        int rollNo;
-        double percent;
 
-    }
-    public static void fun(Student x){
-    System.out.println(x.name);
-    }
     public static void main(String[] args) {
         Student x=new Student(); // declaration and x is the Object
 
         x.name="Mansoor";  // initialization
-        x.rollNo=1234;
         x.percent=92.5;
+//        x.rollNo=1234;
+        System.out.println(x.getRollNo());
 
-        fun(x);
-//        System.out.println(x.name +" "+x.rollNo+" "+x.percent);
+        x.setRollNo(18);
+
     }
 }
