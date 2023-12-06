41ee43668b8b89d65647c4fd669b99f9c5b2cce2
mansoor-P <munaf6007@gmail.com>
2023-12-06 07:08:14 +0530

    chain of three methods in recursion

---
+++ src/com/mansoor/java/killer_java/dsa/recurssion/RecrsiveFuns.java
@@ -0,0 +1,22 @@
+package com.mansoor.java.killer_java.dsa.recurssion;
+
+public class RecrsiveFuns {
+    static void fun1(){
+        int x=1;
+        fun2();
+        System.out.println(x);
+    }
+    static void fun2(){
+       int y=2;
+        fun3();
+        System.out.println(y);
+    }
+    static void fun3(){
+      int  z=3;
+
+        System.out.println(z);
+    }
+    public static void main(String[] args) {
+        fun1();
+    }
+}
