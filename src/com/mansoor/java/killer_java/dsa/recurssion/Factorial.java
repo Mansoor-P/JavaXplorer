d548e144da5e5c63227b97646d3ccc7250d4bd14
mansoor-P <munaf6007@gmail.com>
2023-12-09 08:33:38 +0530

    Factorial number in recursion

--- src/com/mansoor/java/killer_java/dsa/recurssion/Factorial.java
+++ src/com/mansoor/java/killer_java/dsa/recurssion/Factorial.java
@@ -1,19 +1,14 @@
 package com.mansoor.java.killer_java.dsa.recurssion;
-
 public class Factorial {
-    public static int factorialnumber(int n){
-        if (n==0 || n==1){
+    public static int factorialnumber(int n) {
+        if (n == 0 || n == 1) {
             return 1;
         }
-
-        int fact=n*(n-1);
-        int factorial=n*fact;
-        return factorial;
+        int smallAns = factorialnumber(n - 1);    // method-2
+        int fact = n * smallAns;
+        return  fact;
     }
-
     public static void main(String[] args) {
-int n=10;
-int f=factorialnumber(n);
-        System.out.println(f);
+        System.out.println(factorialnumber(10));
     }
 }
