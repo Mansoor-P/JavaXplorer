5aa57cc100bb8b78499ffe62a6655b677d20cb0b
mansoor-P <munaf6007@gmail.com>
2023-12-12 15:53:01 +0530

    Bit Manipulation Practicing

---
+++ src/com/mansoor/java/killer_java/corejava/basics/BitManipulation/DecimalToBinary.java
@@ -0,0 +1,26 @@
+package com.mansoor.java.killer_java.corejava.basics.BitManipulation;
+
+import java.util.Scanner;
+
+public class DecimalToBinary {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int decimal_num=sc.nextInt();
+        int ans=0; // converted to binary number
+
+        int pw=1; /// powers of 10
+
+        while(decimal_num>0){
+            int parity=decimal_num%2;
+
+            ans+=(parity*pw);
+            pw*=10;
+
+            decimal_num/=2;
+        }
+        System.out.println(ans);
+    }
+
+}
