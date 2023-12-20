1222b66fd3023cfb616b4ca9c01ae98ee0a86e7a
mansoor-P <munaf6007@gmail.com>
2023-12-20 19:16:23 +0530

    optimized bubble sort

--- src/com/mansoor/java/killer_java/dsa/sorting/Bubblesort.java
+++ src/com/mansoor/java/killer_java/dsa/sorting/Bubblesort.java
@@ -9,6 +9,8 @@ public class Bubblesort {
         }
     }
     public static void main(String[] args) {
+
+        boolean flag=false;  // has any swapping happens
           int arr []={2,5,7,1,9,10};
         for (int i = 0; i < arr.length-1; i++) {
             for (int j = 0; j < arr.length-i-1; j++) {
@@ -16,7 +18,13 @@ public class Bubblesort {
                     int temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+
+                    flag=true;
                 }
+
+            }
+            if (flag==false){  // have any swaps happened
+                return;
             }
         }
         printArray(arr);
