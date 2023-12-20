46632e9149e2a06ab675bb474c1eb6017dab6718
mansoor-P <munaf6007@gmail.com>
2023-12-20 19:13:16 +0530

    bubble sort

--- src/com/mansoor/java/killer_java/dsa/sorting/Bubblesort.java
+++ src/com/mansoor/java/killer_java/dsa/sorting/Bubblesort.java
@@ -3,24 +3,22 @@ package com.mansoor.java.killer_java.dsa.sorting;
 import java.util.Arrays;
 
 public class Bubblesort {
-//    public static void printArray(int arr[]){
-//        for (int i = 0; i < arr.length; i++) {
-//            System.out.println(arr[i]+" ");
-//        }
-//    }
+    public static void printArray(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]+" ");
+        }
+    }
     public static void main(String[] args) {
           int arr []={2,5,7,1,9,10};
-//        for (int i = 0; i < arr.length-1; i++) {
-//            for (int j = 0; j < arr.length-i-1; j++) {
-//                if (arr[j] > arr[j+1]) {
-//                    int temp=arr[j];
-//                    arr[j]=arr[j+1];
-//                    arr[j+1]=temp;
-//                }
-//            }
-//        }
-        Arrays.sort(arr);
-//        System.out.println(Arrays.toString(arr));
-//        printArray(arr);
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length-i-1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        printArray(arr);
     }
 }
