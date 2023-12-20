512058489e388c71ad671bdedf6eab7c7672f869
mansoor-P <munaf6007@gmail.com>
2023-12-20 20:07:57 +0530

    insertion sort

--- src/com/mansoor/java/killer_java/dsa/sorting/InsertionSort.java
+++ src/com/mansoor/java/killer_java/dsa/sorting/InsertionSort.java
@@ -3,24 +3,26 @@ package com.mansoor.java.killer_java.dsa.sorting;
 import java.util.Arrays;
 
 public class InsertionSort {
-    public static void printArray(int arr[]){
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]+" ");
+    public static void insertionSort(int arr[]){
+        int n=arr.length;
+        for (int i = 1; i < n; i++) {
+            int j=i;
+            while(j> 0 && arr[j]<arr[j-1]){
+                // swap a[j] , a[j-1]
+                int temp=arr[j];
+                arr[j]=arr[j-1];
+                arr[j-1]=temp;
+                j--;
+            }
         }
+
     }
     public static void main(String[] args) {
         int arr []={2,5,7,1,9,10};
+        insertionSort(arr);
         for (int i = 0; i < arr.length; i++) {
-
-            int current=arr[i];
-            int j=i-1;
-            while(j>=0&&current<arr[j]){
-                arr[j+1]=arr[j];
-                j--;
-            }
-            arr[j+1]=current;
+            System.out.println(arr[i]+" ");
         }
-        printArray(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
