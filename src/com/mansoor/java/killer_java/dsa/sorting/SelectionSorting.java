dc449f22f6ece5153167668b33b8b31903d70405
mansoor-P <munaf6007@gmail.com>
2023-12-20 19:41:24 +0530

    selection  sort

--- src/com/mansoor/java/killer_java/dsa/sorting/SelectionSorting.java
+++ src/com/mansoor/java/killer_java/dsa/sorting/SelectionSorting.java
@@ -4,26 +4,32 @@ import java.util.Arrays;
 
 public class SelectionSorting {
 
-            public static void printArray(int arr[]){
+    public static void printArray(int arr[]){
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]+" ");
         }
     }
-        public static void main(String[] args) {
-            int arr []={2,5,7,1,9,10};
-        for (int i = 0; i < arr.length-1; i++) {
-            int smallest =i;
-            for (int j = i+1; j < arr.length; j++) {
-                if (arr[smallest] > arr[j]) {
-                   smallest=j;
+    public static void main(String[] args) {
+        int arr []={2,5,7,1,9,10};
+        int n=arr.length;
+
+        for (int i = 0; i < n-1; i++) {
+            int min_index =i;
+            for (int j = i+1; j < n; j++) {
+                if (arr[min_index] > arr[j]) {
+                    min_index=j;
                 }
-                int temp=arr[smallest];
-                arr[smallest]=arr[i];
+
+                // swap current and minimum element --> current index i will have correct element
+                // a[min_index],  a[i]
+
+                int temp=arr[min_index];
+                arr[min_index]=arr[i];
                 arr[i]=temp;
             }
         }
         printArray(arr);
-            System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr));
         }
     }
 
