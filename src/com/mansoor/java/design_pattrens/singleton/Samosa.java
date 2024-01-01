a16c07167dabe9db6b4e290033acb401123f2df2
mansoor-P <munaf6007@gmail.com>
2024-01-01 12:59:37 +0530

    singleton pattern

--- src/com/mansoor/java/design_pattrens/singleton/Samosa.java
+++ src/com/mansoor/java/design_pattrens/singleton/Samosa.java
@@ -1,25 +1,49 @@
 package com.mansoor.java.design_pattrens.singleton;
-//public class  Samosa {
 
-    public enum Samosa {
-        INSTANCE;
-public void test() { System.out.println("This method from  enum class"); }
+import java.io.Serializable;
 
-//    private static Samosa samosa;
-//    // Constructor
-//   private Samosa(){
+public class  Samosa  implements Serializable,Cloneable {
+
+//    public enum Samosa {
+//        INSTANCE;
+//public void test() { System.out.println("This method from  enum class"); }
+
+    private static Samosa samosa;
+    // Constructor
+   private Samosa(){
 //       if (samosa!= null){
 //           throw new RuntimeException("You are trying to break the singleton pattern");
 //       }
-//   }
-//    // lazy way of  creating singleton object
-//    public static Samosa getSamosa(){
-//       //object of this class
-//      synchronized (Samosa.class){
-//          if(samosa==null){
-//              samosa=new Samosa();
-//          }
-//      }
-//        return samosa;
-//    }
+   }
+    // lazy way of  creating singleton object
+    public static Samosa getSamosa(){
+       //object of this class
+     if(samosa==null){
+         synchronized (Samosa.class){
+             if(samosa==null){
+                 samosa=new Samosa();
+             }
+         }
+     }
+        return samosa;
+    }
+
+    public Object readResolve() {
+        return samosa;
+    }
+    @Override
+    public Object clone() throws CloneNotSupportedException{
+
+//       return samosa;
+       return super.clone();
+    }
 }
+
+/*
+*  singleton pattern rules
+*
+* 1.constructor private
+* 2.object create with the help of method
+* 3.create field to store object is private
+*
+* */
--- src/com/mansoor/java/design_pattrens/singleton/Test.java
+++ src/com/mansoor/java/design_pattrens/singleton/Test.java
@@ -1,9 +1,13 @@
 package com.mansoor.java.design_pattrens.singleton;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.lang.reflect.Constructor;
 
 public class Test {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception ,CloneNotSupportedException{
 
 //        // Lazy way
 //        Samosa samosa1=Samosa.getSamosa();
@@ -20,7 +24,6 @@ public class Test {
 //        System.out.println("jalebi1 gets : "+ " " +jalebi1.hashCode());
 
 
-
         // ways to break singleton pattern
         /*
          * I.Reflection api breaks singleton pattern
@@ -29,19 +32,37 @@ public class Test {
          *           2-> use enum
          *
          * II. Deserialization
+         *      solution :
+         *          Implements readResolve() method
+         *
+         * III.cloning the object
          * */
 
+//        Samosa s1=Samosa.INSTANCE;
+////        Samosa s1=Samosa.getSamosa();
+//        System.out.println(s1.hashCode());
+//
+////        s1.test();
+//        Constructor<Samosa> constructor=Samosa.class.getDeclaredConstructor();
+//        constructor.setAccessible(true);
+//        Samosa s2=constructor.newInstance();
+//        System.out.println(s2.hashCode());
 
-        Samosa s1=Samosa.INSTANCE;
-//        Samosa s1=Samosa.getSamosa();
-        System.out.println(s1.hashCode());
+        Samosa samosa=Samosa.getSamosa();
+        System.out.println(samosa.hashCode());
 
-//        s1.test();
-        Constructor<Samosa> constructor=Samosa.class.getDeclaredConstructor();
-        constructor.setAccessible(true);
-        Samosa s2=constructor.newInstance();
-        System.out.println(s2.hashCode());
+//        ObjectOutputStream oos=new ObjectOutputStream(new FileOutputStream("abc.ob"));
+//        oos.writeObject(samosa);
+//
+//        System.out.println("Serialization Done");
+//
+//        ObjectInputStream ois=new ObjectInputStream(new FileInputStream("abc.ob"));
+//        Samosa s2=(Samosa)ois.readObject();
+//
+//        System.out.println("Deserialization Done");
 
+        Samosa s3=(Samosa) samosa.clone();
+        System.out.println(s3.hashCode());
 
     }
 }
