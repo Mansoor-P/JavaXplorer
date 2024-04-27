1d4d29388ad04a9ca2acb3e504fd1414aa9d806c
Mansoor-P <munaf6007@gmail.com>
2024-04-27 08:30:14 +0530

    misc

--- src/com/mansoor/java/buildTools/mavenproject/my-util/src/main/java/org/example/utilities/MyUtils.java
+++
@@ -1,8 +0,0 @@
-package org.example.utilities;
-
-public class MyUtils {
-    public static void printname(String name){
-        System.out.println("Hello there "+name+" Welcome");
-    }
-
-}
--- src/com/mansoor/java/buildTools/mavenproject/src/main/java/org/example/MavenDemo.java
+++
@@ -1,7 +0,0 @@
-package com.mansoor.java.buildTools.mavenproject.src.main.java.org.example;
-
-public class MavenDemo {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
