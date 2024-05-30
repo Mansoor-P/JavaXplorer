8e14ebf6514e38c5a363eff11e44d34fa4455d2c
Mansoor-P <munaf6007@gmail.com>
2024-05-30 22:35:31 +0530

    InOrderTraversal

---
+++ src/com/mansoor/java/killer_java/dsa/trees/InOrderTraversal.java
@@ -0,0 +1,40 @@
+package com.mansoor.java.killer_java.dsa.trees;
+
+public class InOrderTraversal {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    void inOrder(Node node) {
+        if (node == null) return;
+
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+        inOrder(node.right);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.left.right.left = new Node(6);
+        root.right.left = new Node(7);
+        root.right.right = new Node(8);
+        root.right.right.left = new Node(9);
+        root.right.right.right = new Node(10);
+
+        InOrderTraversal traversal = new InOrderTraversal();
+        System.out.print("Pre-order traversal: ");
+        traversal.inOrder(root);
+    }
+}
